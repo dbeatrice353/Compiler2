73c112f5599301e2b357dba6f1f56fe0f80cd591
David Beatrice <dbeatrice353@gmail.com>
2017-02-19 19:59:21 -0500

    reserved key words list

---
+++ parser.py
@@ -0,0 +1,3 @@
+
+
+reserved_key_words = ['program','is','begin','procedure','end','global','in','out','inout','integer','bool','char','string','float','if','then','else','loop','return','not','false','true']
