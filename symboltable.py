055d5007158726c23cb1620242fb3d937e3f6411
David Beatrice <dbeatrice353@gmail.com>
2017-04-02 12:48:40 -0400

    add argument info

--- symboltable.py
+++ symboltable.py
@@ -59,17 +59,22 @@ class SymbolTable:
                 symbol = self._create_symbol_from_variable_declaration(child)
             else: # procedure_declaration
                 symbol = self._create_symbol_from_procedure_declaration(child)
+                self.populate(child.children[1]) # check procedure body
             if node.token and node.token.value_matches('global'):
                 symbol['global'] = True;
-            self._validate_and_save(symbol)
+            if not self._validate_and_save(symbol):
+                self._report_attempted_redefinition(symbol['identifier'], symbol['definition_line'])
         else:
             for child in node.children:
                 self.populate(child)
 
+        if node.name_matches('procedure_declaration'):
+            self._scope_stack.pop()
+
     def printable_string(self):
-        string = '%-20s%-20s%-20s%-20s%-20s\n'%('identifier','type','data_type','array_length','scope')
+        string = '%-20s%-20s%-20s%-20s%-20s%-20s%-20s\n'%('identifier','type','data_type','array_length','scope','is_argument','direction')
         for s in self._symbols:
-            string += '%-20s%-20s%-20s%-20s%-20s\n'%(str(s['identifier']),str(s['type']),str(s['data_type']),str(s['array_length']),str(s['scope']))
+            string += '%-20s%-20s%-20s%-20s%-20s%-20s%-20s\n'%(str(s['identifier']),str(s['type']),str(s['data_type']),str(s['array_length']),str(s['scope']),str(s['is_argument']),str(s['direction']))
         return string
 
     def _new_symbol(self):
@@ -78,11 +83,11 @@ class SymbolTable:
             'type': '',
             'scope':'',
             'data_type':'',
-            'array_length':None
+            'array_length':None,
+            'direction':None,
+            'is_argument':False,
+            'definition_line':-1
         }
-
-    def _report_error(self, message):
-        print "SYMBOL ERROR: " + message
 
     def _is_valid(self,symbol):
         return self.fetch(symbol['identifier'],symbol['scope']) == None
@@ -90,15 +95,17 @@ class SymbolTable:
     def _validate_and_save(self,symbol):
         if self._is_valid(symbol):
             self._symbols.append(symbol)
+            return True
         else:
-            self._report_error("")
             self._errors = True
+            return False
 
     def _create_symbol_from_variable_declaration(self, variable_declaration):
         symbol = self._new_symbol()
         symbol['scope'] = self._scope_stack.as_string()
         symbol['data_type'] = variable_declaration.children[0].token.value
         symbol['identifier'] = variable_declaration.children[1].token.value
+        symbol['definition_line'] = variable_declaration.children[1].token.line
         if len(variable_declaration.children) >= 3:
             symbol['type'] = 'array'
             symbol['array_length'] = variable_declaration.children[2].token.value
@@ -114,6 +121,7 @@ class SymbolTable:
         symbol['type'] = 'procedure'
         symbol['scope'] = self._scope_stack.as_string()
         symbol['identifier'] = procedure_header.children[0].token.value
+        symbol['definition_line'] = procedure_header.children[0].token.line
         self._scope_stack.push(symbol['identifier'])
         if len(procedure_header.children) >= 2:
             parameter_list = procedure_header.children[1]
@@ -121,11 +129,19 @@ class SymbolTable:
                 parameter = parameter_list.children[0]
                 variable_declaration = parameter.children[0]
                 arg_symbol = self._create_symbol_from_variable_declaration(variable_declaration)
+                arg_symbol['is_argument'] = True
+                arg_symbol['direction'] = parameter.token.value
                 self._validate_and_save(arg_symbol)
                 if len(parameter_list.children) > 1:
                     parameter_list = parameter_list.children[1]
                 else:
                     break
-        self.populate(procedure_body)
-        self._scope_stack.pop()
         return symbol
+
+
+    def _report_error(self,message,line):
+        print "SEMANTIC ERROR (line %s): %s"%(str(line), message)
+
+    def _report_attempted_redefinition(self,name,line_no):
+        message = "Attempted redefinition of symbol \'%s\' in present scope."%(name)
+        self._report_error(message,line_no)
