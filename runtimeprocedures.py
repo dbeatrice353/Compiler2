9acab3dfa492693911521c365560fffaf8dbd2c9
David Beatrice <dbeatrice353@gmail.com>
2017-04-06 20:43:38 -0400

    symbol table entries for runtime procs

---
+++ runtimeprocedures.py
@@ -0,0 +1,202 @@
+SYMBOL_TABLE_ENTRIES = [
+        {
+            'identifier':'getchar',
+            'type': 'procedure',
+            'scope':'main',
+            'data_type':'',
+            'array_length':None,
+            'direction':None,
+            'is_argument':False,
+            'definition_line':-1
+        },
+        {
+            'identifier':'putchar',
+            'type': 'procedure',
+            'scope':'main',
+            'data_type':'',
+            'array_length':None,
+            'direction':None,
+            'is_argument':False,
+            'definition_line':-1
+        },
+        {
+            'identifier':'getstring',
+            'type': 'procedure',
+            'scope':'main',
+            'data_type':'',
+            'array_length':None,
+            'direction':None,
+            'is_argument':False,
+            'definition_line':-1
+        },
+        {
+            'identifier':'putstring',
+            'type': 'procedure',
+            'scope':'main',
+            'data_type':'',
+            'array_length':None,
+            'direction':None,
+            'is_argument':False,
+            'definition_line':-1
+        },
+        {
+            'identifier':'getinteger',
+            'type': 'procedure',
+            'scope':'main',
+            'data_type':'',
+            'array_length':None,
+            'direction':None,
+            'is_argument':False,
+            'definition_line':-1
+        },
+        {
+            'identifier':'putinteger',
+            'type': 'procedure',
+            'scope':'main',
+            'data_type':'',
+            'array_length':None,
+            'direction':None,
+            'is_argument':False,
+            'definition_line':-1
+        },
+        {
+            'identifier':'getfloat',
+            'type': 'procedure',
+            'scope':'main',
+            'data_type':'',
+            'array_length':None,
+            'direction':None,
+            'is_argument':False,
+            'definition_line':-1
+        },
+        {
+            'identifier':'putfloat',
+            'type': 'procedure',
+            'scope':'main',
+            'data_type':'',
+            'array_length':None,
+            'direction':None,
+            'is_argument':False,
+            'definition_line':-1
+        },
+        {
+            'identifier':'getbool',
+            'type': 'procedure',
+            'scope':'main',
+            'data_type':'',
+            'array_length':None,
+            'direction':None,
+            'is_argument':False,
+            'definition_line':-1
+        },
+        {
+            'identifier':'putbool',
+            'type': 'procedure',
+            'scope':'main',
+            'data_type':'',
+            'array_length':None,
+            'direction':None,
+            'is_argument':False,
+            'definition_line':-1
+        },
+        {
+            'identifier':'val',
+            'type': 'variable',
+            'scope':'main/getchar',
+            'data_type':'char',
+            'array_length':None,
+            'direction':'out',
+            'is_argument':True,
+            'definition_line':-1
+        },
+        {
+            'identifier':'val',
+            'type': 'variable',
+            'scope':'main/putchar',
+            'data_type':'char',
+            'array_length':None,
+            'direction':'in',
+            'is_argument':True,
+            'definition_line':-1
+        },
+        {
+            'identifier':'val',
+            'type': 'variable',
+            'scope':'main/getstring',
+            'data_type':'string',
+            'array_length':None,
+            'direction':'out',
+            'is_argument':True,
+            'definition_line':-1
+        },
+        {
+            'identifier':'val',
+            'type': 'variable',
+            'scope':'main/putstring',
+            'data_type':'string',
+            'array_length':None,
+            'direction':'in',
+            'is_argument':True,
+            'definition_line':-1
+        },
+        {
+            'identifier':'val',
+            'type': 'variable',
+            'scope':'main/getinteger',
+            'data_type':'integer',
+            'array_length':None,
+            'direction':'out',
+            'is_argument':True,
+            'definition_line':-1
+        },
+        {
+            'identifier':'val',
+            'type': 'variable',
+            'scope':'main/putinteger',
+            'data_type':'integer',
+            'array_length':None,
+            'direction':'in',
+            'is_argument':True,
+            'definition_line':-1
+        },
+        {
+            'identifier':'val',
+            'type': 'variable',
+            'scope':'main/getfloat',
+            'data_type':'float',
+            'array_length':None,
+            'direction':'out',
+            'is_argument':True,
+            'definition_line':-1
+        },
+        {
+            'identifier':'val',
+            'type': 'variable',
+            'scope':'main/putfloat',
+            'data_type':'float',
+            'array_length':None,
+            'direction':'in',
+            'is_argument':True,
+            'definition_line':-1
+        },
+        {
+            'identifier':'val',
+            'type': 'variable',
+            'scope':'main/getbool',
+            'data_type':'bool',
+            'array_length':None,
+            'direction':'out',
+            'is_argument':True,
+            'definition_line':-1
+        },
+        {
+            'identifier':'val',
+            'type': 'variable',
+            'scope':'main/putbool',
+            'data_type':'bool',
+            'array_length':None,
+            'direction':'in',
+            'is_argument':True,
+            'definition_line':-1
+        }
+]
