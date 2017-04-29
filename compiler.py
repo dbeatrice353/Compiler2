5b9862cf287d6ed6cab4b60a4b8fdf2034013ec4
David Beatrice <dbeatrice353@gmail.com>
2017-04-29 16:56:25 -0400

    comment out debug code

--- compiler.py
+++ compiler.py
@@ -6,49 +6,41 @@ from unvectorizer import Unvectorizer
 from codegen import CodeGenerator
 import sys
 
+output_file = "ir.ll"
 
 def print_out(path,data):
     with open(path,'w') as f:
         f.write(data)
 
-# get the source file path
 file_name = sys.argv[1]
-
-# read the source file
 with open(file_name,'r') as f:
     code = f.read()
 
-# keep track of whether errors occur at any stages
 errors = False
 
-# instantiate everything
 scanner = Scanner()
 parser = Parser()
 symbol_table = SymbolTable()
 semantic_analyzer = SemanticAnalyzer()
+unvectorizer = Unvectorizer()
 code_generator = CodeGenerator()
 
-# scan
 tokens = scanner.scan(code)
-print_out(file_name+'.scan.temp','\n'.join([t.as_string() for t in tokens]))
-
-# parse
+#print_out(file_name+'.scan.temp','\n'.join([t.as_string() for t in tokens]))
 parse_tree = parser.parse(tokens)
 errors = errors or parser.errors()
-print_out(file_name+'.parse.temp',parse_tree.printable_string())
-
-# create a symbol table
+#print_out(file_name+'.parse.temp',parse_tree.printable_string())
 if not errors:
     symbol_table.populate(parse_tree)
     errors = errors or symbol_table.errors()
-    print_out(file_name+'.symbols.temp',symbol_table.printable_string())
-
-# perform semantic analysis
+    #print_out(file_name+'.symbols.temp',symbol_table.printable_string())
 if not errors:
     semantic_analyzer.analyze(parse_tree,symbol_table)
     errors = errors or semantic_analyzer.errors()
-    print_out(file_name+'.operations.temp',semantic_analyzer.printable_string())
-
-# generate the LLVM IR
+    #print_out(file_name+'.operations.temp',semantic_analyzer.printable_string())
 if not errors:
-    code_generator.generate(parse_tree, symbol_table)
+    unvectorizer.unvectorize(parse_tree, symbol_table)
+    #print_out(file_name+'.parse.temp',parse_tree.printable_string())
+    output_code = code_generator.generate(parse_tree, symbol_table)
+    with open(output_file,'w') as f:
+        f.write(output_code)
